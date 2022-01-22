2f93a6eedec9f91224f065453d5201ca60bb2838
amirrezafahimi <amirrezafahimi98@gmail.com>
2022-01-22 11:13:13 +0330

    added make reservation form handler

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"encoding/gob"
 	"fmt"
 	"github.com/alexedwards/scs/v2"
 	"github.com/amirrezafahimi/bookings/internal/config"
 	"github.com/amirrezafahimi/bookings/internal/handlers"
+	"github.com/amirrezafahimi/bookings/internal/models"
 	"github.com/amirrezafahimi/bookings/internal/render"
 	"log"
 	"net/http"
@@ -18,6 +20,9 @@ var session *scs.SessionManager
 
 // main is the main application function
 func main() {
+	// what am I going to put in the session
+	gob.Register(models.Reservation{})
+
 	// TODO: change this to true when in production
 	app.InProduction = false
 
